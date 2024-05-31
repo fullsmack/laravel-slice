cd71ddc41f2bab1716651af62c35d7b2e3bf9dc8
nicolaib <nicolaibaaring@outlook.com>
2024-05-31 15:11:04 +0200

    Updates namespace

--- src/Feature.php
+++ src/Feature.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Fullsmack\LaravelSlice;
+namespace FullSmack\LaravelSlice;
 
 use FullSmack\LaravelSlice\Slice;
 
--- src/Slice.php
+++ src/Slice.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace FullSmack\LaravelSlice;
 
-use Fullsmack\LaravelSlice\Feature;
+use FullSmack\LaravelSlice\Feature;
 
 class Slice
 {
